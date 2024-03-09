672ffad977edf541392be29a8cb65e9aca285594
Anjini2004 <anjini.jamwal@gmail.com>
2024-03-09 18:47:47 -0500

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          sure
+          Hi
         </p>
         <a
           className="App-link"
